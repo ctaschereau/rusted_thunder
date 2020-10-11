ad651b305b6f2cb7ab9b4260e3a23b67e22b7ba7
Cédric Taschereau <cedric.taschereau@boreal-is.com>
2020-10-10 22:47:47 -0400

    Remove old comments. Add TODO.

--- src/main.rs
+++ src/main.rs
@@ -28,169 +28,6 @@ mod config;
 // TODO : Use all_data.gui_settings.gui_distance_units // km/hr
 const KM_PER_MILES: f64 = 1.6;
 
-
-
-
-/*
-struct RustedThunderApp {
-    builder: gtk::Builder,
-    #[allow(dead_code)]
-    client: TeslaClient,
-    vclient: VehicleClient,
-    all_data: FullVehicleData,
-}
-
-impl RustedThunderApp {
-    fn new(builder: gtk::Builder, client: TeslaClient, vclient: VehicleClient, all_data: FullVehicleData) -> Rc<RustedThunderApp> {
-        let instance = Rc::new(RustedThunderApp {
-            builder: builder,
-            client: client,
-            vclient: vclient,
-            all_data: all_data,
-        });
-        instance
-    }
-*/
-    /*
-    fn set_button_labels(&self) {
-        let climate_control_button: gtk::Button = self.builder.get_object("climate_control_button").unwrap();
-        if self.all_data.climate_state.is_auto_conditioning_on {
-            climate_control_button.set_label("Turn climate control OFF");
-        } else {
-            climate_control_button.set_label("Turn climate control ON");
-        }
-
-        let lock_button: gtk::Button = self.builder.get_object("lock_button").unwrap();
-        if self.all_data.vehicle_state.locked {
-            lock_button.set_label("Unlock");
-        } else {
-            lock_button.set_label("Lock");
-        }
-    }
-    */
-
-    /*
-    fn set_buttons(&self, cloned_self: Rc<RustedThunderApp>) {
-        let climate_control_button: gtk::Button = self.builder.get_object("climate_control_button").unwrap();
-        climate_control_button.connect_clicked(move |_button| {
-            cloned_self.on_climate_control_button_clicked(_button);
-        });
-        */
-        // TODO: How can I have set_buttons, set_buttons2 and set_buttons3 be the same function without having "value used here after move" problems?
-        /*
-        let frunk_button: gtk::Button = self.builder.get_object("frunk_button").unwrap();
-        frunk_button.connect_clicked(move |_button| {
-            cloned_self.on_frunk_button_clicked(_button);
-        });
-        let lock_button: gtk::Button = self.builder.get_object("lock_button").unwrap();
-        lock_button.connect_clicked(move |_button| {
-            cloned_self.on_lock_button_clicked(_button);
-        });
-        */
-    //}
-
-    /*
-    fn set_buttons2(&self, cloned_self: Rc<RustedThunderApp>) {
-        let frunk_button: gtk::Button = self.builder.get_object("frunk_button").unwrap();
-        frunk_button.connect_clicked(move |_button| {
-            cloned_self.on_frunk_button_clicked(_button);
-        });
-    }
-
-    fn set_buttons3(&self, cloned_self: Rc<RustedThunderApp>) {
-        let lock_button: gtk::Button = self.builder.get_object("lock_button").unwrap();
-        lock_button.connect_clicked(move |_button| {
-            cloned_self.on_lock_button_clicked(_button);
-        });
-    }
-
-    fn set_doors_and_windows_state(&self) {
-        let vehicle_state: &VehicleState = &self.all_data.vehicle_state;
-
-        let rear_trunk_open_image: gtk::Image = self.builder.get_object("rear_trunk_open_image").unwrap();
-        rear_trunk_open_image.set_opacity(vehicle_state.rt as f64);
-        let front_trunk_open: gtk::Image = self.builder.get_object("front_trunk_open").unwrap();
-        front_trunk_open.set_opacity(vehicle_state.ft as f64);
-        let passenger_front_door_open: gtk::Image = self.builder.get_object("passenger_front_door_open").unwrap();
-        passenger_front_door_open.set_opacity(vehicle_state.pf as f64);
-        let passenger_rear_door_open: gtk::Image = self.builder.get_object("passenger_rear_door_open").unwrap();
-        passenger_rear_door_open.set_opacity(vehicle_state.pr as f64);
-        let driver_front_door_open: gtk::Image = self.builder.get_object("driver_front_door_open").unwrap();
-        driver_front_door_open.set_opacity(vehicle_state.df as f64);
-        let driver_rear_door_open: gtk::Image = self.builder.get_object("driver_rear_door_open").unwrap();
-        driver_rear_door_open.set_opacity(vehicle_state.dr as f64);
-        let passenger_front_window_open: gtk::Image = self.builder.get_object("passenger_front_window_open").unwrap();
-        passenger_front_window_open.set_opacity(vehicle_state.fp_window as f64);
-        let passenger_rear_window_open: gtk::Image = self.builder.get_object("passenger_rear_window_open").unwrap();
-        passenger_rear_window_open.set_opacity(vehicle_state.rp_window as f64);
-        let driver_front_window_open: gtk::Image = self.builder.get_object("driver_front_window_open").unwrap();
-        driver_front_window_open.set_opacity(vehicle_state.fd_window as f64);
-        let driver_rear_window_open: gtk::Image = self.builder.get_object("driver_rear_window_open").unwrap();
-        driver_rear_window_open.set_opacity(vehicle_state.rd_window as f64);
-    }
-
-    fn set_battery_state(&self) {
-        let charge_state: &StateOfCharge = &self.all_data.charge_state;
-
-        let battery_indicator_bar: gtk::LevelBar = self.builder.get_object("battery_indicator_bar").unwrap();
-        battery_indicator_bar.set_value(charge_state.battery_level as f64 / 100.0);
-        battery_indicator_bar.add_offset_value("medium", 0.50);
-        let battery_level_label: gtk::Label = self.builder.get_object("battery_level_label").unwrap();
-        let charging_label: gtk::Label = self.builder.get_object("charging_label").unwrap();
-
-        let nb_remaining_kms = (charge_state.battery_range * KM_PER_MILES) as i32;
-        let charge_level_string = nb_remaining_kms.to_string() + "km";
-        battery_level_label.set_text(charge_level_string.as_str());
-        let charging_label_text: String;
-        match charge_state.charging_state.as_str() {
-            "Disconnected" => charging_label_text = String::from(""),
-            "Complete" => charging_label_text = String::from("Charging complete"),
-            "Charging" => {
-                let nb_minutes_until_full = (charge_state.time_to_full_charge * 60.0) as i32;
-                charging_label_text = format!("Charging... {} minutes remaining", nb_minutes_until_full);
-            }
-            _ => charging_label_text = charge_state.charging_state.clone()
-        }
-        charging_label.set_text(charging_label_text.as_str());
-    }
-
-    fn on_climate_control_button_clicked(&self, _button: &gtk::Button) {
-        if self.all_data.climate_state.is_auto_conditioning_on {
-            match self.vclient.auto_conditioning_stop() {
-                // TODO : Should I log the _v variable if _.result != true ?
-                Ok(_v) => println!("auto_conditioning has been stopped."),
-                Err(e) => println!("failed to stop the auto_conditioning: {:?}", e),
-            }
-        } else {
-            match self.vclient.auto_conditioning_start() {
-                Ok(_v) => println!("auto_conditioning has been turned on."),
-                Err(e) => println!("failed to start the auto_conditioning: {:?}", e),
-            }
-        }
-    }
-
-    fn on_frunk_button_clicked(&self, _button: &gtk::Button) {
-        println!("on_frunk_button_clicked!");
-        //TODO: POST /api/1/vehicles/{id}/command/actuate_trunk
-    }
-
-    fn on_lock_button_clicked(&self, _button: &gtk::Button) {
-        if self.all_data.vehicle_state.locked {
-            match self.vclient.door_unlock() {
-                // TODO : Should I log the _v variable if _.result != true ?
-                Ok(_v) => println!("doors have been unlocked."),
-                Err(e) => println!("failed to unlock the doors: {:?}", e),
-            }
-        } else {
-            match self.vclient.door_lock() {
-                Ok(_v) => println!("doors have been locked."),
-                Err(e) => println!("failed to lock the doors: {:?}", e),
-            }
-        }
-    }
-    */
-//}
-
 fn main() {
     init_logger();
     info!("rusted_thunder starting up...");
@@ -217,10 +54,7 @@ fn init_logger() {
         .init();
 }
 
-fn build_ui(app: &gtk::Application) {
-    let glade_src = include_str!("app_layout.glade");
-    let builder: Rc<gtk::Builder> = Rc::new(gtk::Builder::from_string(glade_src));
-
+fn init_css_provider() {
     let provider = gtk::CssProvider::new();
     provider
         .load_from_path("../style/main.css")
@@ -230,6 +64,13 @@ fn build_ui(app: &gtk::Application) {
         &provider,
         gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
     );
+}
+
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("app_layout.glade");
+    let builder: Rc<gtk::Builder> = Rc::new(gtk::Builder::from_string(glade_src));
+
+    init_css_provider();
 
     let loading_banner: gtk::Revealer = builder.get_object("loading_banner").unwrap();
     loading_banner.show_all();
@@ -366,6 +207,9 @@ fn set_buttons(builder: Rc<gtk::Builder>, tx_to_comm: mpsc::Sender<MessagesForWo
 }
 
 fn on_refresh_button_clicked(tx_to_comm: &mut mpsc::Sender<MessagesForWorker>) {
+    // TODO :
+    //let loading_banner: gtk::Revealer = builder.get_object("loading_banner").unwrap();
+    //loading_banner.show_all();
     match tx_to_comm.send(MessagesForWorker::DoRefresh()) {
         Ok(_) => {}
         Err(err) => error!("{:?}", err)
